1984455b11b6b958f080cf059dbd95b25c0f0f57
Manuel Lerchner <manuel.lerchner1111@gmail.com>
2023-02-24 23:55:03 +0100

    update pipeline

--- render.sh
+++ render.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-pandoc -V documentclass=report -V classoption=oneside -V geometry:vmargin=14.5mm -V lang:de --fail-if-warnings --output=merge.pdf md/*.md
+mkdir -p docs
+pandoc -V documentclass=report -V classoption=oneside -V geometry:vmargin=14.5mm -V lang:de --fail-if-warnings --output=docs/numerisches_programmieren_IN0019_WS2223_merge.pdf md/*.md
